9d01f5271d8a3ff00afec520773640a63f351381
Felix Palludan Hargreaves <felix.palludan.hargreaves@LEGO.com>
2016-10-09 21:28:04 +0200

    Add multi-grammars. List grammar example using recursion.

--- src/main/scala-2.11/FreeAppGrammar.scala
+++ src/main/scala-2.11/FreeAppGrammar.scala
@@ -10,6 +10,7 @@ sealed trait GrammarA[A]
 case class Regx[T](key: Regex)                                   extends GrammarA[T]
 case class Optional[T](grammar: Free[GrammarA, T])               extends GrammarA[T]
 case class Choice[T](g: Free[GrammarA, T], f: Free[GrammarA, T]) extends GrammarA[T]
+case class Multi[T](g: Free[GrammarA, T])                        extends GrammarA[T]
 
 object FreeAppGrammar extends App {
 
@@ -24,18 +25,30 @@ object FreeAppGrammar extends App {
   def optional[T](g: Grammar[T]): Grammar[T] =
     liftF(Optional[T](g))
 
+  def multi[T](g: Grammar[T]): Grammar[T] =
+    liftF(Multi[T](g))
+
   implicit class GrammarOps[T](g: Grammar[T]) {
     def ~(h: Grammar[T]): Grammar[T] = for (a <- g; b <- h) yield b
-    def * : Grammar[T]               = optional(g)
+    def ? : Grammar[T]               = optional(g)
     def |(h: Grammar[T]): Grammar[T] = choice(g, h)
+    def + : Grammar[T]               = multi(g)
   }
 
   def grammar: Grammar[String] = {
     val aOrB: Grammar[String]   = regx("a".r) | regx("b".r)
     val one: Grammar[String]    = regx("1".r)
-    val maybe2: Grammar[String] = regx("2".r).*
+    val maybe2: Grammar[String] = regx("2".r).?
     val three: Grammar[String]  = regx("3".r)
     one ~ maybe2 ~ three ~ aOrB
+  }
+
+  def listGrammar = {
+    val openBracket = regx("\\[".r)
+    val digit       = regx("\\d".r)
+    val endBracket  = regx("\\]".r)
+    val moreDigits  = (regx(",".r) ~ digit).+
+    openBracket ~ digit ~ (moreDigits).? ~ endBracket
   }
 
   val testStrings = Seq(
@@ -49,8 +62,11 @@ object FreeAppGrammar extends App {
     "13b42"
   )
 
+  parseAndPresent(regx("7".r).+)("7778")
+  parseAndPresent(listGrammar)("[5,4,10]")
+
   def parseAndPresent(g: Grammar[String])(s: String) = {
-    val parseResult = grammar.foldMap(parserInterpreter).run(s).value
+    val parseResult = g.foldMap(parserInterpreter).run(s).value
     println(s"$s:  $parseResult")
   }
 
@@ -84,8 +100,20 @@ object FreeAppGrammar extends App {
     new (GrammarA ~> ParserInterpreterState) {
       def apply[A](fa: GrammarA[A]): ParserInterpreterState[A] =
         fa match {
-          case Regx(key)   => parseRegex(key)
-          case Optional(b) => parseOptional(b.foldMap(this))
+          case Regx(regexp) => parseRegex(regexp)
+          case Optional(b)  => parseOptional(b.foldMap(this))
+          case m @ Multi(g) =>
+            def x: State[String, A] =
+              State.apply(state => {
+                g.foldMap(this)
+                  .run(state)
+                  .map {
+                    case (s, ParseSuccess(_))    => x.run(s).value
+                    case r @ (s, ParseFailure()) => (s, ParseSuccess(s).asInstanceOf[A])
+                  }
+                  .value
+              })
+            x
           case Choice(a, b) =>
             State.apply(state => {
               val runA = a.foldMap(this).run(state).value
@@ -96,7 +124,6 @@ object FreeAppGrammar extends App {
               }
             })
         }
-
     }
 
 }
